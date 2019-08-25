d9b167114d513069679feb8ce42405c4f56bcf88
huy nguyen <huykgit98@gmail.com>
2019-08-25 12:33:54 +0700

    Xay dung cau truc project

---
+++ toeiclearning/src/main/java/com/huynguyen/toeiclearning/ToeiclearningApplication.java
@@ -0,0 +1,13 @@
+package com.huynguyen.toeiclearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToeiclearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToeiclearningApplication.class, args);
+	}
+
+}
---
+++ toeiclearning/src/main/java/com/huynguyen/toeiclearning/entities/VaiTro.java
@@ -0,0 +1,5 @@
+package com.huynguyen.toeiclearning.entities;
+
+public class VaiTro {
+
+}
